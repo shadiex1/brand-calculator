e00f89c85b34960fb7e54fdb131b15e94d41430b
unknown <shadiex@hotmail.com>
2021-11-18 02:20:59 +0200

    added full app

---
+++ src/App.jsx
@@ -0,0 +1,168 @@
+import React, { Component } from "react";
+import styles from "./App.module.scss";
+import { NextArrow } from "./svg";
+import Carousel from "nuka-carousel";
+import Img1 from "./assets/brex-inc-logo-vector.png";
+import Img2 from "./assets/Stripe-Logo-png-hd.png";
+import Img3 from "./assets/amex.png";
+import card from "./assets/card.png";
+class App extends Component {
+  state = {
+    vendor: 8,
+    media: 8,
+    corpSpend: 8,
+    teamProject: 8,
+    brands: [
+      { multiply: 0.3, img: Img1, name: "brex" },
+      { multiply: 2.9, img: Img2, name: "stripe" },
+      { multiply: 1.3, img: Img3, name: "amex" },
+    ],
+    selectedBrand: null,
+  };
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  setSelectedBrand = (brand) => {
+    this.setState({
+      selectedBrand: brand,
+    });
+  };
+  render() {
+    const { vendor, media, corpSpend, teamProject, selectedBrand } = this.state;
+    const total =
+      Number(vendor) + Number(media) + Number(corpSpend) + Number(teamProject);
+    return (
+      <div className={styles.App}>
+        <div className={styles.calculator}>
+          <h2>Calculate your Rewards</h2>
+          <div className={styles.input}>
+            <label for="vendor"> Vendor/Bills</label>
+            <input
+              type="number"
+              name="vendor"
+              id="vendor"
+              placeholder="Enter a number"
+              onChange={this.handleChange}
+              value={this.state.vendor}
+            />
+          </div>
+          <div className={styles.input}>
+            <label for="media"> Media/Ad Spend</label>
+            <input
+              type="number"
+              name="media"
+              id="media"
+              placeholder="Enter a number"
+              onChange={this.handleChange}
+              value={this.state.media}
+            />
+          </div>
+          <div className={styles.input}>
+            <label for="corpSpend"> Corp.Spend</label>
+            <input
+              type="number"
+              name="corpSpend"
+              id="corpSpend"
+              placeholder="Enter a number"
+              onChange={this.handleChange}
+              value={this.state.corpSpend}
+            />
+          </div>
+          <div className={styles.input}>
+            <label for="teamProject"> Team Project</label>
+            <input
+              type="number"
+              name="teamProject"
+              id="teamProject"
+              placeholder="Enter a number"
+              onChange={this.handleChange}
+              value={this.state.teamProject}
+            />
+          </div>
+        </div>
+        {vendor && media && corpSpend && teamProject && (
+          <div className={styles.rewards}>
+            <div className={styles.compare}>
+              <h2>Compare your Rewards</h2>
+              <div className={styles.slider}>
+                <Carousel
+                  wrapAround
+                  cellSpacing={5}
+                  defaultControlsConfig={{
+                    prevButtonClassName: `${styles.prev}`,
+                    nextButtonClassName: `${styles.next}`,
+                    pagingDotsStyle: {
+                      display: "none",
+                    },
+                  }}
+                  slidesToShow="3"
+                  renderCenterLeftControls={({ previousSlide }) => (
+                    <button className={styles.prev} onClick={previousSlide}>
+                      <NextArrow />
+                    </button>
+                  )}
+                  renderCenterRightControls={({ nextSlide }) => (
+                    <button className={styles.next} onClick={nextSlide}>
+                      <NextArrow />{" "}
+                    </button>
+                  )}
+                >
+                  {this.state.brands.map((brand, i) => (
+                    <div
+                      style={
+                        selectedBrand && brand.name === selectedBrand.name
+                          ? { transform: "" }
+                          : null
+                      }
+                      onClick={() => this.setSelectedBrand(brand)}
+                      key={i}
+                      className={styles.brandCard}
+                    >
+                      <img src={brand.img} alt="Logo" />
+                    </div>
+                  ))}
+                </Carousel>
+              </div>
+              <div className={styles.rewards}>
+                <img src={card} />
+                <div className={styles.total}>
+                  {selectedBrand && (
+                    <div className={styles.progress}>
+                      <span>{selectedBrand.name}</span>
+                      <div
+                        style={{ width: `${selectedBrand.multiply * 20}%` }}
+                        className={styles.progressbar}
+                      ></div>{" "}
+                      ${total * selectedBrand.multiply}{" "}
+                    </div>
+                  )}
+
+                  <div className={styles.progress}>
+                    <span>Unlimited Rewards</span>
+                    <div
+                      style={{ width: `${1.1 * 20}%` }}
+                      className={styles.progressbar}
+                    ></div>{" "}
+                    ${total * 1.1}
+                  </div>
+                  <div className={styles.progress}>
+                    <span>Lifetime Rewards</span>
+                    <div
+                      style={{ width: `${1.2 * 20}%` }}
+                      className={styles.progressbar}
+                    ></div>{" "}
+                    ${total * 1.2}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/svg.js
@@ -0,0 +1,10 @@
+export const NextArrow = (props) => (
+    <svg id="back" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
+    <g id="Group_4" data-name="Group 4">
+      <g id="Group_3" data-name="Group 3">
+        <path id="Path_7" data-name="Path 7" d="M15,30A15,15,0,1,1,30,15,15.016,15.016,0,0,1,15,30ZM15,2.1A12.9,12.9,0,1,0,27.9,15,12.913,12.913,0,0,0,15,2.1Z" fill="#fff"/>
+        <path id="Path_8" data-name="Path 8" d="M185.218,171.131a1.536,1.536,0,0,1-2.168-2.177l5.376-5.376L183.05,158.2a1.539,1.539,0,0,1,2.177-2.177l6.46,6.46a1.536,1.536,0,0,1,0,2.168Z" transform="translate(-171.877 -148.582)" fill="#fff"/>
+      </g>
+    </g>
+  </svg>
+)
